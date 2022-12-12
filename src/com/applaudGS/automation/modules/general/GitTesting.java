f34bad35e47ed92b90eef3e84b8ed74d71420469
uvreddymsm@gmail.com <12Test@12>
2022-12-12 09:26:31 +0530

    Completed Script For Refund Transactions

---
+++ src/com/applaudGS/automation/modules/general/GitTesting.java
@@ -0,0 +1,11 @@
+package com.applaudGS.automation.modules.general;
+
+import org.testng.annotations.Test;
+
+public class GitTesting {
+
+    @Test
+    public void refundTransactions(){
+        System.out.println("This Mehtod Is For Refund Transactions");
+    }
+}
